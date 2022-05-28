895dc17ee50574244671492e72f0c38d57b3f50f
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-05-28 23:28:10 +0200

    create routes in express and routes filter

--- WorkingWithExpress/app.js
+++ WorkingWithExpress/app.js
@@ -1,8 +1,19 @@
-const http = require('http')
 const express = require('express')
+const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin.js')
+const shopRouter = require('./routes/shop.js')
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: false}))
 
-const server = http.createServer(app)
+app.use('/admin', adminRoutes)
+app.use(shopRouter)
 
-server.listen(3000)
+
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1> Page not found 404</h1>')
+})
+
+
+app.listen(3000)
