bc67ce7b9926d01c5e9d87498b4880f6a367fcdc
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-05-28 19:52:27 +0200

    create routers file with routerrs inside of it

--- UnderstandingTheBasics/app.js
+++ UnderstandingTheBasics/app.js
@@ -1,37 +1,6 @@
 const http = require('http')
-const fs = require('fs')
+const { requestHandler } = require('./routes.js')
 
-
-const server = http.createServer((req,res) => {
-
-  if(req.url === '/' && req.method === 'GET'){
-    res.setHeader("Content-Type", "text/html");
-    res.write('<form action="/message" method="POST"> <input type="text" name="message"> <button type="submit>Send</button> </form>')
-    return res.end()
-  }
-  if(req.url === '/message' && req.method === 'POST'){
-    const body = [];
-    req.on('data', (chunk) =>{  
-      body.push(chunk)
-    })
-    req.on('end', () =>{
-      const parseBody = Buffer.concat(body).toString()
-      const message = parseBody.split('=')[1]
-      fs.writeFile('message.txt',message, err => {
-        res.statusCode = 302
-        res.setHeader('Location','/')
-        return res.end()
-      })
-    })
-
-
-    
-  }
-
-
-  res.setHeader("Content-Type", "text/html");
-  res.write('<h1> Default Route </h1>')
-  res.end()
-});
+const server = http.createServer(requestHandler)
 
 server.listen(3000)
---
+++ UnderstandingTheBasics/routes.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+
+const requestHandler = (req,res) => {
+  if(req.url === '/' && req.method === 'GET'){
+    res.setHeader("Content-Type", "text/html");
+    res.write('<form action="/message" method="POST"> <input type="text" name="message"> <button type="submit>Send</button> </form>')
+    return res.end()
+  }
+  if(req.url === '/message' && req.method === 'POST'){
+    const body = [];
+    req.on('data', (chunk) =>{  
+      body.push(chunk)
+    })
+    req.on('end', () =>{
+      const parseBody = Buffer.concat(body).toString()
+      const message = parseBody.split('=')[1]
+      fs.writeFile('message.txt',message, () => {
+        res.statusCode = 302
+        res.setHeader('Location','/')
+        return res.end()
+      })
+    })
+  
+  
+    
+  }
+  
+  
+  res.setHeader("Content-Type", "text/html");
+  res.write('<h1> Default Route </h1>')
+  res.end()
+
+}
+
+
+module.exports.requestHandler = {
+  requestHandler
+}
