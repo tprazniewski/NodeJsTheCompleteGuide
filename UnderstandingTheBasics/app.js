edd13e3e45d1e20d2eebd68e5aa9cf56027ccdf1
tprazniewski <104758997+tprazniewski@users.noreply.github.com>
2022-05-28 17:28:55 +0200

    create server with 2 urls,one of them have req.on event listeners to handle the data from the input

---
+++ UnderstandingTheBasics/app.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const fs = require('fs')
+
+
+const server = http.createServer((req,res) => {
+
+  if(req.url === '/' && req.method === 'GET'){
+    res.setHeader("Content-Type", "text/html");
+    res.write('<form action="/message" method="POST"> <input type="text" name="message"> <button type="submit>Send</button> </form>')
+    return res.end()
+  }
+  if(req.url === '/message' && req.method === 'POST'){
+    const body = [];
+    req.on('data', (chunk) =>{  
+      body.push(chunk)
+    })
+    req.on('end', () =>{
+      const parseBody = Buffer.concat(body).toString()
+      const message = parseBody.split('=')[1]
+      fs.writeFileSync('message.txt',message)
+    })
+
+    res.statusCode = 302
+    res.setHeader('Location','/')
+    return res.end()
+
+    
+  }
+
+
+  res.setHeader("Content-Type", "text/html");
+  res.write('<h1> Default Route </h1>')
+  res.end()
+});
+
+server.listen(3000)
